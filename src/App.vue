0156cbfa27f63d9014a20e3472782f01b3f784b8
Elia <mail@eliareutlinger.ch>
2019-09-19 12:10:35 +0200

    TOVO Template

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,60 @@
 <template>
-    <b-container>
-        <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
-            <p>For more information visit our website</p>
-            <b-btn variant="primary" href="https://bootstrap-vue.js.org/">More Info</b-btn>
-        </b-jumbotron>
+  <div id="app">
 
-        <b-form-group horizontal :label-cols="4" description="Let us know your name." label="Enter your name">
-            <b-form-input v-model.trim="name"></b-form-input>
-        </b-form-group>
+    <!-- Preloader -->
+    <div class="loader-wrapper">
+      <div class="loader"></div>
+    </div>
+    <!-- Preloader end -->
 
-        <b-alert variant="success" :show="showAlert">Hello {{ name }}</b-alert>
-    </b-container>
+    <router-view name="navbar"></router-view>
+    <router-view/>
+    <vue-progress-bar></vue-progress-bar>
+  </div>
 </template>
-
 <script>
-export default {
+  import $ from 'jquery'
+  export default {
     name: 'App',
-
-    data () {
-        return {
-            name: ''
-        }
+    mounted () {
+      //  [App.vue specific] When App.vue is finish loading finish the progress bar
+      this.$Progress.finish()
+      this.loader()
     },
-
-    computed: {
-        showAlert () {
-            return this.name.length > 4 ? true : false
+    created () {
+      //  [App.vue specific] When App.vue is first loaded start the progress bar
+      this.$Progress.start()
+      //  hook the progress bar to start before we move router-view
+      this.$router.beforeEach((to, from, next) => {
+        //  does the page we want to go to have a meta.progress object
+        if (to.meta.progress !== undefined) {
+          let meta = to.meta.progress
+          // parse meta tags
+          this.$Progress.parseMeta(meta)
         }
+        //  start the progress bar
+        this.$Progress.start()
+        //  continue to next page
+        next()
+      })
+      //  hook the progress bar to finish after we've finished moving router-view
+      this.$router.afterEach((to, from) => {
+        //  finish the progress bar
+        this.$Progress.finish()
+      })
+    },
+    methods: {
+      loader () {
+        $(document).ready(function () {
+          // ----------------------------------------
+          //  Pre Loader
+          // ----------------------------------------
+          $(window).on('load', function () {
+            $('.loader-wrapper').fadeOut('slow')
+            $('.loader-wrapper').remove('slow')
+          })
+        })
+      }
     }
-
-}
+  }
 </script>
