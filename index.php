21144ee3057f397982c27bc6ac61d01c835c7e16
Elia <mail@eliareutlinger.ch>
2019-09-20 12:36:39 +0200

    lang EN

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
 if ($lang === "de") {
-    header("Location: de/");
+    header("Location: en/");
 } else {
     header("Location: en/");
 }
