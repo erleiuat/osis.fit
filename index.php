3af166ba998f1d08902e4cb78d5bb71020f2f0fd
Elia <mail@eliareutlinger.ch>
2019-10-03 16:26:07 +0200

    impro

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
-header("Content-Type: text/html; charset=utf-8");
+header('Content-Type: text/html; charset=UTF-8');
+
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
 if ($lang === "de") {
